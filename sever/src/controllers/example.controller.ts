b843e2f8cab432a1dfec760bf9a227dd47410bca
Peera-27 <crbrsonline@gmail.com>
2024-11-13 12:06:21 +0700

    ืnew

---
+++ sever/src/controllers/example.controller.ts
@@ -0,0 +1,26 @@
+import Elysia, { t } from "elysia"
+
+export const example = new Elysia()
+    .get("/", () => "Hello Elysia", {
+        detail: {
+            tags: ["Example"],
+            summary: "Get Hello Elysia",
+            description: "i am Elysia"
+        }
+    })
+    .post("/about", ({ body }) => {
+        return {
+            id: 'wasa',
+            msg: 'nuania ' + body.name
+        }
+    }, {
+        body: t.Object({
+            name: t.String()
+        }),
+        detail: {
+            tags: ["Example"],
+            summary: "About",
+            description: "i am About"
+        }
+    }
+    )
