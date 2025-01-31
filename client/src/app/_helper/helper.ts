12ca7fe451186b76d5a3ef0c48fbeca07d749a3c
Peera-27 <crbrsonline@gmail.com>
2025-01-31 15:21:42 +0700

    Memberservice

--- client/src/app/_helper/helper.ts
+++ client/src/app/_helper/helper.ts
@@ -1,3 +1,4 @@
+import { QueryPagination, UserQueryPagination } from "../_models/pagination"
 import { User } from "../_models/user"
 
 const defaultAvatar = '/assets/DefaultAvatar.png'
@@ -22,4 +23,29 @@ export function pareUserPhoto(user: User): User {
     user.avatar = getAvatar(user)
     user.photoOfTheDay = getPhotoOfTheDay(user)
     return user
+}
+export function pareQuery(query: QueryPagination | UserQueryPagination): string {
+    let querystring = '?'
+    if (query.pageSize)
+        querystring += `&pageSize=${query.pageSize}`
+    if (query.currentPage)
+        querystring += `&currentPage=${query.currentPage}`
+
+    if ('username' in query && query.username) {
+        querystring += `&username=${query.username}`
+
+    }
+    if ('looking_for' in query && query.looking_for) {
+        querystring += `&looking_for=${query.looking_for}`
+
+    }
+    if ('min_age' in query && query.min_age) {
+        querystring += `&min_age=${query.min_age}`
+
+    }
+    if ('max_age' in query && query.max_age) {
+        querystring += `&max_age=${query.max_age}`
+
+    }
+    return querystring
 }
--- client/src/app/member/member.component.ts
+++ client/src/app/member/member.component.ts
@@ -1,14 +1,25 @@
-import { Component, inject } from '@angular/core'
+import { Component, inject, OnInit, WritableSignal } from '@angular/core'
 import { environment } from '../../environments/environment'
 import { HttpClient } from '@angular/common/http'
 import { Router } from '@angular/router'
-
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MemberService } from '../_services/member.service'
+import { default_pageSizeOption, Paginator, UserQueryPagination } from '../_models/pagination'
+import { User } from '../_models/user'
 @Component({
   selector: 'app-member',
-  imports: [],
+  imports: [MatPaginatorModule],
   templateUrl: './member.component.html',
   styleUrl: './member.component.scss'
 })
-export class MemberComponent {
-
+export class MemberComponent implements OnInit {
+  private memberservice = inject(MemberService)
+  paginator: WritableSignal<Paginator<UserQueryPagination, User>>
+  pageSize = default_pageSizeOption
+  constructor() {
+    this.paginator = this.memberservice.paginator
+  }
+  ngOnInit(): void {
+    this.memberservice.getMembers()
+  }
 }
